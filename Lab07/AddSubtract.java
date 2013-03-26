5074361aaa58d1775ffa0704d0dbbf218b165253
Nyefan <st.auant@gmail.com>
2013-03-26 14:38:09 -0500

    Comment AddSubtract.java

--- Lab07/AddSubtract.java
+++ Lab07/AddSubtract.java
@@ -1,3 +1,15 @@
+/*--------------------------------------------------------------*
+ *  File Name: AddSubtract.java                                 |
+ *  Author: Anthony Davis St. Aubin				|
+ *  KUID: 248****						|
+ *  Email Address: a162s797@ku.edu				|
+ *  Lab Number:  07                                             |
+ *  Description: An introduction to methods; requests two	|
+ *    integers from the user and outputs the sum in two 	|
+ *    ways.							|
+ *  Last Changed: 26-March-2013					|
+ ---------------------------------------------------------------*/
+
 import java.util.Scanner;
 
 public class AddSubtract {
@@ -8,22 +20,23 @@ public class AddSubtract {
 		do {
 			int x = getInput("1st");
 			int y = getInput("2nd");
-			addSubtractIntegers(x,y);
-		
-			System.out.println("\nNow you are calling 2 different methods, add and sub, which return values as");
+			addSubtractIntegers(x,y); // Outputs the sum and difference of x and y and notifies the user that the operation has been handled inside the method;
+			
+			//Outputs the sum and difference of x and y, notifying the user that the operations have been handled seperately from within main;
+			System.out.println("\nNow you are calling 2 different methods, addIntegers and subtractIntegers, which return values as");
 			int sum = addIntegers(x,y);
 			int diff = subtractIntegers(x,y);
 			System.out.println("Sum=" + sum + "\nDifference=" + diff);
 		
-			shouldLoop = checkShouldLoop();
+			shouldLoop = checkShouldLoop(); // Asks the user if they would like to continue looping;
 		} while (shouldLoop);
 		
 	}
 	
-	public static void addSubtractIntegers(int x,int y) {
+	public static void addSubtractIntegers(int x,int y) { //Prints the values of (x + y) and (x - y) with appropriate notifiers;
 		int sum = addIntegers(x,y);
 		int diff = subtractIntegers(x,y);
-		System.out.println("You are inside the Method called addSub, where Sum=" + sum + " and Difference=" + diff);
+		System.out.println("You are inside the Method called addSubtractIntegers, where Sum=" + sum + " and Difference=" + diff);
 	}
 	
 	public static int addIntegers(int x,int y) {
@@ -34,12 +47,12 @@ public class AddSubtract {
 		return x - y;
 	}
 	
-	public static int getInput(String x) {
+	public static int getInput(String x) { //Requests and returns the user's input
 		System.out.println("Please enter the " + x + " No:");
 		return keyboard01.nextInt();
 	}
 	
-	public static boolean checkShouldLoop() {
+	public static boolean checkShouldLoop() { //Asks the user if the program should continue to loop and returns the appropriate boolean representation
 		System.out.println("\nIt was fun, right?!  Want to do it again? (y=1/n=0)");
 		char chkChar = keyboard01.next().charAt(0);
 		
