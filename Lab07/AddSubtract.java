734e32b5750c36b19efa83ac79fe73c2ecb46f52
Nyefan <st.auant@gmail.com>
2013-03-26 15:03:42 -0500

    Update AddSubtract.java

--- Lab07/AddSubtract.java
+++ Lab07/AddSubtract.java
@@ -20,23 +20,26 @@ public class AddSubtract {
 		do {
 			int x = getInput("1st");
 			int y = getInput("2nd");
-			addSubtractIntegers(x,y); // Outputs the sum and difference of x and y and notifies the user that the operation has been handled inside the method;
+			addSubtractIntegers(x,y); // Note[01];
 			
-			//Outputs the sum and difference of x and y, notifying the user that the operations have been handled seperately from within main;
-			System.out.println("\nNow you are calling 2 different methods, addIntegers and subtractIntegers, which return values as");
+			// Note[02];
+			System.out.println("" +
+				"\nNow you are calling 2 different methods, addIntegers and subtractIntegers, which return values as");
 			int sum = addIntegers(x,y);
 			int diff = subtractIntegers(x,y);
-			System.out.println("Sum=" + sum + "\nDifference=" + diff);
+			System.out.println("Sum=" + sum + 
+				"\nDifference=" + diff);
 		
 			shouldLoop = checkShouldLoop(); // Asks the user if they would like to continue looping;
 		} while (shouldLoop);
 		
 	}
 	
-	public static void addSubtractIntegers(int x,int y) { //Prints the values of (x + y) and (x - y) with appropriate notifiers;
+	public static void addSubtractIntegers(int x,int y) { // Prints the values of (x + y) and (x - y) with appropriate notifiers;
 		int sum = addIntegers(x,y);
 		int diff = subtractIntegers(x,y);
-		System.out.println("You are inside the Method called addSubtractIntegers, where Sum=" + sum + " and Difference=" + diff);
+		System.out.println("You are inside the Method called addSubtractIntegers, where Sum=" + sum + 
+			" and Difference=" + diff);
 	}
 	
 	public static int addIntegers(int x,int y) {
@@ -47,18 +50,19 @@ public class AddSubtract {
 		return x - y;
 	}
 	
-	public static int getInput(String x) { //Requests and returns the user's input
+	public static int getInput(String x) { // Requests and returns the user's input
 		System.out.println("Please enter the " + x + " No:");
 		return keyboard01.nextInt();
 	}
 	
-	public static boolean checkShouldLoop() { //Asks the user if the program should continue to loop and returns the appropriate boolean representation
+	public static boolean checkShouldLoop() { // Note[01];
 		System.out.println("\nIt was fun, right?!  Want to do it again? (y=1/n=0)");
 		char chkChar = keyboard01.next().charAt(0);
 		
 		if (chkChar == '1' || chkChar == 'y' || chkChar == 'Y') {return true;}
 		else if (chkChar == '0' || chkChar == 'n' || chkChar == 'N') {
-			System.out.println("\nEnough of Addition and Subtraction.  I'm not in elementary school anymore!");
+			System.out.println("\nEnough of Addition and Subtraction." +
+				"I'm not in elementary school anymore!");
 			return false;
 		}
 		else {
@@ -68,3 +72,16 @@ public class AddSubtract {
 		
 	}
 }
+
+/**--------------------------------------------NOTES--------------------------------------------
+ * main.01
+ * 		Outputs the sum and difference of x and y and notifies the user that the operation has 
+ * 		been handled inside the method;
+ * main.o2
+ * 		Outputs the sum and difference of x and y, notifying the user that the operations have 
+ * 		been handled seperately from within main;
+ * 
+ * checkShouldLoop.01
+ * 		KNOWN BUG: any input followed by a hard return will be processed according to its first
+ * 		letter, resulting in entries like "yar" and "nargles" acting as 'y' and 'n' respectively
+ */
