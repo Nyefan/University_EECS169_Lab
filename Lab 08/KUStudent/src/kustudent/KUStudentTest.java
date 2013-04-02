10d14139b66115a05d9e7e5a0467d6e291f50f92
Nyefan <st.auant@gmail.com>
2013-04-02 14:32:15 -0500

    Comment KUStudentTest.java

--- Lab 08/KUStudent/src/kustudent/KUStudentTest.java
+++ Lab 08/KUStudent/src/kustudent/KUStudentTest.java
@@ -21,31 +21,46 @@ public class KUStudentTest {
 
   public void main(String[] args) {
 
-    requestStudentName(); // Asks the user what student's fname and lname should be, specifying the format (fname lname);
-    setName();            // Takes the user's input and assigns the student's name through KUStudent's set_name method;
-    requestKUInfo();      // Asks the user what student's KUID and dpt should be, specifying the format (KUID dpt);
-    setKUInfo();          // Takes the user's input and assigns the student's KUInfo through KUStudent's set_kuinfo method;
+    requestStudentName(); // Note[01]
+    setName();            // Note[02]
+    requestKUInfo();      // Note[03]
+    setKUInfo();          // Note[04]
 
     student.displayInfo();
   }
 
-  private static void requestStudentName() {
+  private static void requestStudentName() { // Note[main.01];
     System.out.println("Please input the student's name (firstname lastname):");
   }
 
-  private static void setName() {
+  private static void setName() { // Note[main.02];
     String fname = keyboard01.next();
     String lname = keyboard01.next();
     student.set_name(fname,lname);
   }
 
-  private static void requestKUInfo() {
+  private static void requestKUInfo() { // Note[main.03];
     System.out.println("Please input the student's KUID and department (KUID dpt):");
   }
 
-  private static void setKUInfo() {
+  private static void setKUInfo() { // Note[main.04];
     String kuid = keyboard01.next();
     String dpt = keyboard01.next();
     student.set_kuinfo(kuid, dpt);
   }
 }
+
+/**--------------------------------NOTES--------------------------------
+ * main.01
+ *              Asks the user what student's fname and lname should be, 
+ *              specifying the format (fname lname);
+ * main.o2
+ * 		        Takes the user's input and assigns the student's name 
+ *              through KUStudent's set_name method;
+ * main.03
+ * 		        Asks the user what student's KUID and dpt should be,
+ *              specifying the format (KUID dpt);
+ * main.04
+ *              Takes the user's input and assigns the student's KUInfo 
+ *              through KUStudent's set_kuinfo method;
+ */
